170d5a3dd467cb4c7f1046c78130c6438fa99757
arthur <zhiyuan@gmail.com>
2024-05-15 21:14:37 -0700

    checked in v2

--- v2/ch10.go
+++ v2/ch10.go
@@ -1,8 +1,15 @@
 package main
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+    constraints.Integer | constraints.Float
+}
+
 // Add adds two input integers and returns the sum of them
 //
 // For more information, refer to [https://www.mathsisfun.com/numbers/addition.html]
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
     return a + b
 }
+
---
+++ v2/go.mod
@@ -0,0 +1,5 @@
+module github.com/Nekokan1500/ch10
+
+go 1.22.1
+
+require golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842
